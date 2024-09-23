6b2d96236c8c081d2ccdd8a4a664ca96a2d0be53
dihego <dihegov@gmail.com>
2024-09-23 01:15:40 -0400

    Update ExportDNSfromADtoRoute.py
    adding a additional zones for support

--- ExportDNSfromADtoRoute.py
+++ ExportDNSfromADtoRoute.py
@@ -13,21 +13,19 @@ DC2IP = input("Enter the IP Secondary Domain Controler x.x.x.x/32: \n")
 print ("#####" * 20 )
 print ("#####" * 20 )
 
-'''
-Query DNS for active records
-Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName _msdcs.dgoptam.com
-Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName dgoptam.com
+#Query DNS for active records
+#Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName _msdcs.dgoptam.com
+#Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName dgoptam.com
 
-AD by default doesnt clear cache in 7 days. If your environtment is dynamic changing those values "Domain AGING" to something appropiate 1 day perhaps.
-* Clear Cache and RELOAD to get the latest.
-* For Linux domains if they dont show up. YOu may want to rejoin if the hostname was set after joining to AD.
+#AD by default doesnt clear cache in 7 days. If your environtment is dynamic changing those values "Domain AGING" to something appropiate 1 day perhaps.
+#* Clear Cache and RELOAD to get the latest.
+#* For Linux domains if they dont show up. YOu may want to rejoin if the hostname was set after joining to AD.
 
-the AD commands will export in CSV format the records of the domain ZONES. 
+#the AD commands will export in CSV format the records of the domain ZONES. 
 #export zone in files: replace dgoptam.com 
-Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName dgoptam.com | export-csv C:\Users\dgoadmin\Desktop\dnsrecords.csv
-Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName _msdcs.dgoptam.com | export-csv C:\Users\dgoadmin\Desktop\dnsrecords_msdcs.csv
+#Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName dgoptam.com | export-csv C:\Users\dgoadmin\Desktop\dnsrecords.csv
+#Get-DnsServerResourceRecord -ComputerName ip-c613016e.dgoptam.com -ZoneName _msdcs.dgoptam.com | export-csv C:\Users\dgoadmin\Desktop\dnsrecords_msdcs.csv
 
-'''
 
 print ("Enter the list of records from the CSV ")
 print ("example::  From column Hostname ## _ldap._tcp.ForestDnsZones	SRV ###")
@@ -57,8 +55,8 @@ for addttl in cleanlistofrecords:
             print (f"{srvrecord} 0 100 3268 {DC1} ")
             print (f"{srvrecord} 0 100 3268 {DC2} ")     
         elif '_ldap' in srvrecord:
-            print (f"{srvrecord} 0 100 3389 {DC1} ")
-            print (f"{srvrecord} 0 100 3389 {DC2} ") 
+            print (f"{srvrecord} 0 100 389 {DC1} ")
+            print (f"{srvrecord} 0 100 389 {DC2} ") 
         elif 'kpasswd' in srvrecord:
             print (f"{srvrecord} 0 100 464 {DC1} ")
             print (f"{srvrecord} 0 100 464 {DC2} ") 
@@ -69,6 +67,12 @@ for addttl in cleanlistofrecords:
 
 print (f"{DC1} 300 A {DC1IP}")
 print (f"{DC2} 300 A {DC2IP}")
+
+print (f"_ldap._udp 300 SRV 0 100 389 {DC1}")
+print (f"_ldap._udp 300 SRV 0 100 389 {DC2}")
+
+print (f"_kpasswd 300 SRV 0 100 464 {DC1}")
+print (f"_kpasswd 300 SRV 0 100 464 {DC2}")
 
 print ()
 print ()
@@ -93,6 +97,7 @@ print ("#####" * 20 )
 print ("#####" * 20 )
 
 cleanlistofrecords2ndDOMAIN = list((set(secondnddomainlistcmds)))
+
 for addttl2ndDOMAIN in cleanlistofrecords2ndDOMAIN:
     if 'SRV' in addttl2ndDOMAIN:
         srvrecord2ndDOMAIN =  (addttl2ndDOMAIN.replace("SRV", "300 SRV "))
@@ -100,9 +105,18 @@ for addttl2ndDOMAIN in cleanlistofrecords2ndDOMAIN:
             print (f"{srvrecord2ndDOMAIN} 0 100 88 {DC1} ")
             print (f"{srvrecord2ndDOMAIN} 0 100 88 {DC2} ")
         elif '_ldap' in srvrecord2ndDOMAIN:
-            print (f"{srvrecord2ndDOMAIN} 0 100 3389 {DC1} ")
-            print (f"{srvrecord2ndDOMAIN} 0 100 3389 {DC2} ") 
+            print (f"{srvrecord2ndDOMAIN} 0 100 389 {DC1} ")
+            print (f"{srvrecord2ndDOMAIN} 0 100 389 {DC2} ") 
+
+print (f"_kerberos._udp.dc. 300 SRV 0 100 88 {DC1}")
+print (f"_kerberos._udp.dc. 300 SRV 0 100 88 {DC2}")
+
+print (f"_ldap._udp.dc. 300 SRV 0 100 389 {DC1}")
+print (f"_ldap._udp.dc. 300 SRV 0 100 389 {DC2}")
 
 
-print (f"gc 300 A {DC1IP}")
-print (f"gc 300 A {DC2IP}")
+print (f"gc. 300 A {DC1IP}")
+print (f"gc. 300 A {DC2IP}")
+
+print (f"{DC1}300 A {DC1IP} ")
+print (f"{DC2}300 A {DC2IP} ")
